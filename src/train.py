5ee60e4d99f9c09730f0d95f8cf3d34590900222
Vitor Salomao <vitorsalomao0601@gmail.com>
2025-03-26 20:10:11 -0700

    :sparkles: start commit

---
+++ src/train.py
@@ -0,0 +1,11 @@
+# list of that cards that have been played
+# number of cards each player has
+# the order the game is going
+# the cards on the player's hand
+
+# Starter
+# The cards on the player's hand
+# The number of cards each player has
+
+def generate_train_data():
+    pass
